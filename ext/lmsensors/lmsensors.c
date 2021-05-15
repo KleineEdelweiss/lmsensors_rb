19ff6377fbb581c55c58b7f44377816a029457b7
G.S. Fordham <kleine.edelweiss@outlook.com>
2021-05-15 08:30:15 -0400

    Dramatically rewrote current initialization of the module; created Sensors class under LmSensors module; use module-level variable to handle initialization, as sensors_init and sensors_cleanup affect ALL instances

--- ext/lmsensors/lmsensors.c
+++ ext/lmsensors/lmsensors.c
@@ -1,5 +1,5 @@
 // ext/lmsensors/lmsensors.c
-/* Last backed-up version: < 2021-May-14 @ 06:34:37*/
+/* Last backed-up version: < 2021-May-15 @ 08:26:07 */
 #include <ruby.h>
 #include <stdbool.h>
 #include <string.h>
@@ -7,82 +7,195 @@
 
 #define BUFSIZE 1024
 
+#define LMSLOADER sensors_obj* sensor; TypedData_Get_Struct(self, sensors_obj, &sensors_type, sensor);
+
+#define LMSVALIDATE if (!LMS_OPEN) {\
+return rb_str_new2(\
+"ERROR: Sensors were not configured! Run `LmSensors.init [filename|nil]`"); }
+
 VALUE LmSensors = Qnil;
 VALUE Sensors = Qnil;
+VALUE LMS_OPEN = Qfalse;
 
-VALUE method_sensor_get_subfeatures() {
-  return Qnil;
-}
+// ---------------------------
+// LMSENSORS MODULE METHODS BELOW
+// ---------------------------
 
 /*
- * Get the features for a specific chip
+ * sensors_init works ACROSS different sensor objects and
+ * should, therefore, be handled by the module.
  */
-VALUE method_sensor_get_features() {
-  return Qnil;
-}
+VALUE lmsensors_init(VALUE self, VALUE filename) {
+  // First, check if the sensors were already initialized
+  if (LMS_OPEN) {
+    return LMS_OPEN;
+  } // End init check
+  
+  // Inheriting class should override this with either NULL of value
+  FILE *input = NULL;
+  // If no file is chosen, just pass NULL -- usually is NULL
+  if (RB_TYPE_P(filename, T_STRING)) {
+    FILE *input = fopen(StringValueCStr(filename), "r");
+  } // Otherwise, don't try to open, leave NULL
+  
+  int err = 0; // Errors are stored here
+  err = sensors_init(input);
+  if (err) { return LMS_OPEN; } // There was an error -- return Qfalse
+  else {
+    LMS_OPEN = Qtrue;
+    return LMS_OPEN;
+  } // No error -- initialized
+} // End module init
+
+// Cleanup sensors
+VALUE lmsensors_cleanup() {
+  LMS_OPEN = Qfalse;
+  sensors_cleanup();
+  return LMS_OPEN;
+} // End module cleanup
+
+// ---------------------------
+// SENSORS METHODS BELOW
+// ---------------------------
+
+/*
+ * The sensors_obj struct stores
+ * iterable constant fields to be used 
+ * when accessing available chips, as well
+ * as their features and subfeatures.
+ * 
+ * Despite being const, they are actually altered by
+ * pointer to their internal structure, as per the
+ * lmsensors API.
+ */
+typedef struct {
+  const sensors_chip_name *chip_ptr;
+  const sensors_feature *feat_ptr;
+  const sensors_subfeature *subfeat_ptr;
+} sensors_obj; // End sensors object struct
+
+// Free the data in the sensors object
+void sensors_free(void* data) {
+  sensors_cleanup();
+  free(data);
+} // End sensors free
+
+// Get the size of the sensors object
+size_t sensors_size(const void* data) {
+  return sizeof(data);
+} // End size getter
+
+// Encapsulate the sensors object for use with
+// Ruby's side of the code
+static const rb_data_type_t sensors_type = {
+  .wrap_struct_name = "sensors_obj",
+  .function = {
+    .dmark = NULL,
+    .dfree = sensors_free,
+    .dsize = sensors_size,
+  },
+  .data = NULL,
+  .flags = RUBY_TYPED_FREE_IMMEDIATELY,
+}; // End encapsulation
+
+// Allocate space for the object
+VALUE sensors_obj_alloc(VALUE self) {
+  // Allocate memory for object
+  sensors_obj* sensor = malloc(sizeof(sensors_obj));
+  // Wrap it within a sensors type struct
+  return TypedData_Wrap_Struct(self, &sensors_type, sensor);
+} // End memory allocation
+
+// Constructor
+VALUE sensors_initialize(VALUE self) {
+  // Create the pointers
+  sensors_chip_name *chip;
+  sensors_feature *feat;
+  sensors_subfeature *subfeat;
+  
+  // Attach them to struct
+  sensors_obj s = {
+    .chip_ptr = chip,
+    .feat_ptr = feat,
+    .subfeat_ptr = subfeat,
+  };
+  
+  LMSLOADER; // Shorthand load sensor
+  *sensor = s; // Bind the struct
+  return self;
+} // End constructor
 
 /* 
  * Enumerate all the chips that can be read from
  */
-VALUE method_lmsensors_enumerate_chips() {
-  const sensors_chip_name *chip;
+VALUE method_sensors_enumerate_chips(VALUE self) {
+  LMSLOADER; // Shorthand load sensor
   int chip_nr = 0;
   int cnt = 0;
   int err = 0;
   int success = 0;
   
+  // Create temporary buffer for data
   char buffer[BUFSIZE];
   size_t size = BUFSIZE;
   
-  FILE *input = NULL;
-  err = sensors_init(input);
+  LMSVALIDATE; // Shorthand early return, if not loaded
   
+  // If not early return, sensors are valid
   VALUE data = rb_hash_new();
-  while ((chip = sensors_get_detected_chips(NULL, &chip_nr))) {
-    err = sensors_do_chip_sets(chip);
+  while ((sensor->chip_ptr = sensors_get_detected_chips(NULL, &chip_nr))) {
+    err = sensors_do_chip_sets(sensor->chip_ptr);
     if (err) { // If anything but 0, it can't load
       rb_hash_aset(data, rb_id2sym(rb_intern("fatal_err")), rb_str_new2(
         "Cannot load chip sets. Have you already done 'sensors-detect?'"));
       break;
     } else {
       VALUE idx = rb_sprintf("item%d", cnt);
-      const char *adapter = sensors_get_adapter_name(&chip->bus);
-      success = sensors_snprintf_chip_name(buffer, size, chip);
+      const char *adapter = sensors_get_adapter_name(&sensor->chip_ptr->bus);
+      success = sensors_snprintf_chip_name(buffer, size, sensor->chip_ptr);
       if (success > 0) {
         // Create a new chip entry
         VALUE curr_chip = rb_hash_new();
         rb_hash_aset(curr_chip, rb_id2sym(rb_intern("adapter")),
-          rb_str_new2(adapter)); // Attach the adapter
+                     rb_str_new2(adapter)); // Attach the adapter
         // Attach the name of the chip
         rb_hash_aset(curr_chip, rb_id2sym(rb_intern("name")), rb_str_new2(buffer));
         // Get the chip path
         rb_hash_aset(curr_chip, rb_id2sym(rb_intern("path")), 
-          rb_str_new2(chip->path));
+                     rb_str_new2(sensor->chip_ptr->path));
         
         // Add the chip entry to the list
         rb_hash_aset(data, rb_id2sym(rb_intern(StringValueCStr(idx))),
-          curr_chip);
+                     curr_chip);
       } else {
         rb_hash_aset(data, rb_id2sym(rb_intern("chip_error")), idx);
         break;
       }
     }
-    cnt++;
+    cnt++; // Update the count of available chips
   }
-  // Perform the cleanup operations
-  sensors_cleanup();
   
   // Add the total count, before returning the data
   rb_hash_aset(data, rb_id2sym(rb_intern("total_sensors")), INT2NUM(cnt));
   return data;
-}
+} // End enumerate method
 
 /*
  * Initialize the LmSensors wrapper
  */
 void Init_lmsensors() {
-  LmSensors = rb_define_module("LmSensors");
-  Sensors = rb_define_class_under(LmSensors, "Sensors", rb_cData);
-
-  rb_define_module_function(LmSensors, "chips", method_lmsensors_enumerate_chips, 0);
-}
+  LmSensors = rb_define_module("LmSensors"); // Module
+  Sensors = rb_define_class_under(LmSensors, "Sensors", rb_cData); // Main class
+  
+  // Define the global
+  rb_global_variable(&LMS_OPEN);
+  
+  // Module methods
+  rb_define_module_function(LmSensors, "init", lmsensors_init, 1);
+  rb_define_module_function(LmSensors, "cleanup", lmsensors_cleanup, 0);
+  
+  // Sensors methods
+  rb_define_alloc_func(Sensors, sensors_obj_alloc);
+  rb_define_method(Sensors, "initialize", sensors_initialize, 0);
+  rb_define_method(Sensors, "enum", method_sensors_enumerate_chips, 0);
+} // End module and class initialization
